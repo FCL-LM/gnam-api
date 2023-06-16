4c682652ea103d51802d653842ca3c2071da41c0
Herbrant <cdavide98carnemolla@gmail.com>
2023-06-16 20:18:01 +0200

    feat: added healthchecks in the docker image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM rust:alpine as builder
 
+RUN apk add musl-dev
+
 WORKDIR /usr/src/gnam-api/
 COPY . .
 RUN cargo install --path .
 
 FROM alpine:latest
 
+RUN apk add curl
 COPY --from=builder /usr/local/cargo/bin/gnam-api /usr/local/bin/gnam-api
 
+HEALTHCHECK CMD curl --fail http://localhost:9090/health || exit 1   
 ENTRYPOINT ["gnam-api"]
--- src/gnam.rs
+++ src/gnam.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{get, HttpResponse, post};
 use serde::{Deserialize, Serialize};
 pub const APPLICATION_JSON: &str = "application/json";
 
@@ -12,9 +12,21 @@ pub struct TweetRequest {
     pub message: Option<String>,
 }
 
-// Endpoint test
-#[get("/")]
+// healthcheck endpoint
+#[get("/health")]
 pub async fn index() -> HttpResponse {
+    let response = IndexResponse {
+        message: String::from("The gnam-api service is up"),
+    };
+
+    HttpResponse::Ok()
+        .content_type(APPLICATION_JSON)
+        .json(response)
+}
+
+// 
+#[post("/gnam")]
+pub async fn gnam() -> HttpResponse {
     let response = IndexResponse {
         message: String::from("The gnam-api service is up"),
     };
